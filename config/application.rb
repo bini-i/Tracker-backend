d6120091d4b1a773a765581cd8c8475494f11a4a
Binyam <binyam@pop-os.localdomain>
2021-10-08 09:54:31 +0300

    Add devise gem, annotate and pry-rails
    - also configured Application controller for API

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,26 @@
 require_relative "boot"
 
-require "rails/all"
+# frozen_string_literal: true
+
+# rubocop:disable Style/RedundantBegin
+
+require "rails"
+
+%w(
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  action_cable/engine
+  rails/test_unit/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,6 +28,8 @@ Bundler.require(*Rails.groups)
 
 module TrackerBackend
   class Application < Rails::Application
+    config.api_only = true
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
