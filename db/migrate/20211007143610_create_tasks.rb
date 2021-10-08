84e4f87a8060e208a3dcb7db776778e3ba3c4f7f
Binyam <binyam@pop-os.localdomain>
2021-10-08 09:56:01 +0300

    Add task migration

---
+++ db/migrate/20211007143610_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :task_name, null: false
+      t.string :description, null: false
+      t.integer :progress, null: false
+      t.timestamps
+    end
+  end
+end
