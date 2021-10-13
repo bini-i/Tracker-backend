317e36e653275bbeb56b19dd353cf9c6dd466091
Binyam <binyam@pop-os.localdomain>
2021-10-13 06:44:30 +0300

    Add task controller
    - to handle creating, editing and destroying tasks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,8 @@
 class SessionsController < ApplicationController
     def create
-        puts 'params'
-        puts params
-        puts headers
-        # puts current_user
         @user = User.find_by(email: params[:email])
-        
         if @user&.valid_password?(params[:password])
-            # console.log(current_user)s
-            byebug
+            @user.save
             render :create, status: :created
         else
             head(:unauthorized)
