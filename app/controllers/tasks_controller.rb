86cac8ac0a5ad758efeb0baf1f91b303b9ba75f3
Binyam <binyam@pop-os.localdomain>
2021-10-19 09:35:09 +0300

    Update tasks_controller
    - add create action to handle creating tasks along with todos
    - also added json_param_key_transform to handle naming difference
      between camelCase and snake_case

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,24 @@ class TasksController < ApplicationController
     end
 
     def create
-
+        @task = Task.new(
+            task_name: params[:task][:task_name], 
+            description: params[:task][:description], 
+            progress: params[:task][:progress]
+            )
+        puts(@task.task_name)
+        if(@task.save)
+            params[:task][:todos].each do |todo|
+                @task.todos.create(
+                    checked: params[:task][:todos][:checked],
+                    value: params[:task][:todos][:value]
+                    )
+                end
+            puts('saved~~~~~~~~~~')
+            render json: {status: 'OK'}, status: :created 
+        else
+            head(:unprocessable_entity)     
+        end
     end
 
     def show
@@ -14,4 +31,12 @@ class TasksController < ApplicationController
     def destroy
 
     end
+
+    private
+
+    def tasks_params 
+        # todo separate in to two params
+        params.require(:task).permit(:task_name, :description, :progress, todos: [:checked, :value])
+    end
+
 end
